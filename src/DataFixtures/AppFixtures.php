620b3e658c4bb783859fd8f4f70ab87d8e89016a
Naoufel <naoufel.raouine@gmail.com>
2020-11-09 19:58:18 +0100

    Load fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle("A first post!");
+        $blogPost->setPublished(new \DateTime('2020-11-09 14:34:00'));
+        $blogPost->setContent('Post text');
+        $blogPost->setAuthor('Naoufel1434');
+        $blogPost->setSlug('a-first-blog');
+
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle("A second post!");
+        $blogPost->setPublished(new \DateTime('2020-11-09 14:35:00'));
+        $blogPost->setContent('Post text');
+        $blogPost->setAuthor('Naoufel1434');
+        $blogPost->setSlug('a-second-blog');
+
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
